4f9f7eff40e5b8bcf2a391a88e7f8881561e30f8
Peng <wensheng.peng@cotiviti.com>
2023-11-28 21:10:40 -0500

    Add sample schema, JSON and jsonschema2pojo

---
+++ src/main/java/com/mycompany/app/model/PersonSchema.java
@@ -0,0 +1,248 @@
+
+package com.mycompany.app.model;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Person Schema
+ * <p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "name",
+    "age",
+    "email",
+    "address",
+    "isStudent",
+    "grades"
+})
+@Generated("jsonschema2pojo")
+public class PersonSchema {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    private Integer id;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    private String name;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("age")
+    private Integer age;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("address")
+    private Address address;
+    @JsonProperty("isStudent")
+    private Boolean isStudent;
+    @JsonProperty("grades")
+    private List<Integer> grades = new ArrayList<Integer>();
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("age")
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("age")
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("address")
+    public Address getAddress() {
+        return address;
+    }
+
+    @JsonProperty("address")
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @JsonProperty("isStudent")
+    public Boolean getIsStudent() {
+        return isStudent;
+    }
+
+    @JsonProperty("isStudent")
+    public void setIsStudent(Boolean isStudent) {
+        this.isStudent = isStudent;
+    }
+
+    @JsonProperty("grades")
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    @JsonProperty("grades")
+    public void setGrades(List<Integer> grades) {
+        this.grades = grades;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PersonSchema.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("age");
+        sb.append('=');
+        sb.append(((this.age == null)?"<null>":this.age));
+        sb.append(',');
+        sb.append("email");
+        sb.append('=');
+        sb.append(((this.email == null)?"<null>":this.email));
+        sb.append(',');
+        sb.append("address");
+        sb.append('=');
+        sb.append(((this.address == null)?"<null>":this.address));
+        sb.append(',');
+        sb.append("isStudent");
+        sb.append('=');
+        sb.append(((this.isStudent == null)?"<null>":this.isStudent));
+        sb.append(',');
+        sb.append("grades");
+        sb.append('=');
+        sb.append(((this.grades == null)?"<null>":this.grades));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.address == null)? 0 :this.address.hashCode()));
+        result = ((result* 31)+((this.name == null)? 0 :this.name.hashCode()));
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        result = ((result* 31)+((this.grades == null)? 0 :this.grades.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.age == null)? 0 :this.age.hashCode()));
+        result = ((result* 31)+((this.email == null)? 0 :this.email.hashCode()));
+        result = ((result* 31)+((this.isStudent == null)? 0 :this.isStudent.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof PersonSchema) == false) {
+            return false;
+        }
+        PersonSchema rhs = ((PersonSchema) other);
+        return (((((((((this.address == rhs.address)||((this.address!= null)&&this.address.equals(rhs.address)))&&((this.name == rhs.name)||((this.name!= null)&&this.name.equals(rhs.name))))&&((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id))))&&((this.grades == rhs.grades)||((this.grades!= null)&&this.grades.equals(rhs.grades))))&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.age == rhs.age)||((this.age!= null)&&this.age.equals(rhs.age))))&&((this.email == rhs.email)||((this.email!= null)&&this.email.equals(rhs.email))))&&((this.isStudent == rhs.isStudent)||((this.isStudent!= null)&&this.isStudent.equals(rhs.isStudent))));
+    }
+
+}
